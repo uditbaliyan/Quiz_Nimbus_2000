cf47803c9a24ca5b36293f576c7cafd03f5acb8a
uditbaliyan <baliyanvdit@gmail.com>
2026-02-03 16:53:32 +0530

    pre setup

--- quiz_master/leaderboards/apps.py
+++ quiz_master/leaderboards/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class LeaderboardsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'leaderboards'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "leaderboards"
